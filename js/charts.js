476e6f86607fcfcd5da0725f7863f3874b176f58
ante <ante@gmail.com>
2017-07-07 19:01:28 +0200

    Solving charts in JS

--- js/charts.js
+++ js/charts.js
@@ -1,97 +1,238 @@
-var ctx1 = document.getElementById("myChart").getContext("2d");
+const hourlyTraffic = document.getElementById('Hourly-traffic');
+const dailyTraffic = document.getElementById('Daily-traffic');
+const weeklyTraffic = document.getElementById('Weekly-traffic');
+const monthlyTraffic = document.getElementById('Monthly-traffic');
+const barDailyTraffic = document.getElementById('bar-daily-traffic');
+const mobileUsers = document.getElementById('mobile-users');
+const list = document.getElementById("traffic-ul");
+
 
 Chart.defaults.global.defaultFontColor = '#ACACAC';
 
-let chart = new Chart(ctx1, {
-   type: 'line',
-   data: {
-       datasets: [{
-           label: 'Hourly',
-           data: [500, 700, 1000, 1200, 1500, 1700, 2000, 2200, 2300, 2500],
-           backgroundColor: "rgba(116, 119, 191, 0.2)",
-           pointBorderColor:"#8588C6",
-           pointBackgroundColor:"#fff",
-           borderColor:"rgba(131, 134, 199, 0.5)",
-
-       }, {
-            label: 'Daily',
-            data: [100, 400, 800, 700, 900, 1000, 1200, 1300, 1200, 1000],
-            backgroundColor: "rgba(116, 119, 191, 0.2)",
-            pointBorderColor:"#8588C6",
-            pointBackgroundColor:"#fff",
-            borderColor:"rgba(131, 134, 199, 0.5)",
-       }, {
-            label: 'Weekly',
-            data: [400, 500, 600, 800, 1000, 1100, 1300, 1500, 1300, 1100],
-            backgroundColor: "rgba(116, 119, 191, 0.2)",
-            pointBorderColor:"#8588C6",
-            pointBackgroundColor:"#fff",
-            borderColor:"rgba(131, 134, 199, 0.5)",
-       },{
-            label: 'Monthly',
-            data: [1000, 1200, 1100, 1500, 1600, 1800, 2000, 1700, 1900, 2100],
-            backgroundColor: "rgba(116, 119, 191, 0.2)",
-            pointBorderColor:"#8588C6",
-            pointBackgroundColor:"#fff",
-            borderColor:"rgba(131, 134, 199, 0.5)",
-       }],
-       labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24'],
-       fontColor: 'red',
-   },
-   options: {
-     elements: {
-          line: {
-            tension: 0,
-
-          }
-      },
-      legend: {
-        display: false,
-      }
-  }
-});
-
-
-var ctx2 = document.getElementById("myChart-daily").getContext('2d');
-var myChart = new Chart(ctx2, {
-type: 'bar',
-data: {
-  labels: [ "S","M", "T", "W", "T", "F", "S"],
-  datasets: [{
-    data: [80, 100, 150, 150, 100, 90, 80],
-    backgroundColor: "#7377BF",
-  }]
-},
-options: {
-  legend: {
-    display: false,
-
-  }
-}
-});
-
-var ctx3 = document.getElementById("myChart-mobile").getContext('2d');
-
-var myDoughnutChart = new Chart(ctx3, {
-  type: 'doughnut',
+let hourlyChart = new Chart (hourlyTraffic, {
+  type:"line",
   data: {
-    labels:["Phones", "Mobile", "Desktop"],
-    datasets: [{
-      data:[50,100,200],
-      labels: ["Phones", "Mobile", "Desktop"],
-      backgroundColor: ["#74B1BF", "#81C98F", "#7377BF"],
-      lineWidth: 100,
-    }]
-  },
+    labels: ["12 - 5", "5 - 11", "12 - 18", "19 - 12"],
+    datasets: [
+			{
+				fill: false,
+				lineTension: 0,
+				borderColor: "#a111af",
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: "#fff",
+				pointHoverRadius: 4,
+				pointRadius: 3,
+				data: [12,23,32,19]
+			}
+		]
+	},
   options: {
-    legend: {
-      position:"right",
-      labels: {
-                display: false,
-                padding: 20,
-                boxWidth: 15,
-            }
-    },
-
-  }
+		scales: {
+			xAxes: [{
+				gridLines: {
+					color: "#000000",
+					display: false
+				}
+			}],
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				},
+				gridLines: {
+          			color: "#000000",
+          			display: false
+        		}
+			}]
+		},
+		legend: {
+            display: false
+         }
+	}
 });
+
+let dailyChart = new Chart(dailyTraffic, {
+	type: 'line',
+	data: {
+		labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		datasets: [
+			{
+				label: "Daily Traffic",
+				fill: true,
+				lineTension: 0.5,
+				backgroundColor: "#a111af", //$light-purple
+				borderColor: "#72dff7", //$light-blue
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: "#fff",
+				pointBorderWidth: 1,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: "rgba(66,212,244,1)",
+				pointHoverBorderColor: "rgba(220,220,220,1)",
+				pointHoverBorderWidth: 2,
+				pointRadius: 7,
+				pointHitRadius: 10,
+				data: [33,59,78,63,69,55,40]
+			}
+		]
+	},
+	options: {
+		responsive: true,
+		scales: {
+			xAxes: [{
+				gridLines: {
+					color: "#000000",
+					display: false
+				}
+			}],
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				},
+				gridLines: {
+          			color: "#000000",
+          			display: false
+        		}
+			}]
+		},
+		legend: {
+            display: false
+         }
+	}
+});
+
+
+
+
+let weeklyChart = new Chart(weeklyTraffic, {
+	type: 'line',
+	data: {
+		labels: ['1', '2', '3', '4', '5', '6', '7'],
+		datasets: [
+			{
+				label: "Weekly Traffic",
+				fill: true,
+				lineTension: 0.5,
+				backgroundColor: "#e8ff1e", //$yellow
+				borderColor: "#a111af", //$light-purple
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: "#fff",
+				pointBorderWidth: 1,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: "rgba(66,212,244,1)",
+				pointHoverBorderColor: "rgba(220,220,220,1)",
+				pointHoverBorderWidth: 2,
+				pointRadius: 7,
+				pointHitRadius: 10,
+				data: [153,175,164,186, 178, 181, 186]
+			}
+		]
+	},
+	options: {
+		scales: {
+			xAxes: [{
+				gridLines: {
+					color: "#000000",
+					display: false
+				}
+			}],
+			yAxes: [{
+				ticks: {
+					beginAtZero: false
+				},
+				gridLines: {
+          			color: "#000000",
+          			display: false
+        		}
+			}]
+		},
+		legend: {
+            display: false
+         }
+	}
+});
+
+let monthlyChart = new Chart(monthlyTraffic, {
+	type: 'line',
+	data: {
+		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+		datasets: [
+			{
+				label: "Weekly Traffic",
+				fill: true,
+				lineTension: 0.5,
+				backgroundColor: "#a111af", //$light-purple
+				borderColor: "#e8ff1e", //$yellow
+				borderCapStyle: 'butt',
+				borderDash: [],
+				borderDashOffset: 0.0,
+				borderJoinStyle: 'miter',
+				pointBorderColor: "#fff",
+				pointBorderWidth: 1,
+				pointHoverRadius: 5,
+				pointHoverBackgroundColor: "rgba(66,212,244,1)",
+				pointHoverBorderColor: "#e8ff1e",
+				pointHoverBorderWidth: 2,
+				pointRadius: 7,
+				pointHitRadius: 10,
+				data: [433,459,494,533,606,578,612,633]
+			}
+		]
+	},
+	options: {
+		scales: {
+			xAxes: [{
+				gridLines: {
+					color: "#000000",
+					display: false
+				}
+			}],
+			yAxes: [{
+				ticks: {
+					beginAtZero: false
+				},
+				gridLines: {
+          			color: "#000000",
+          			display: false
+        		}
+			}]
+		},
+		legend: {
+            display: false
+         }
+	}
+});
+
+dailyTraffic.style.display = "none";
+weeklyTraffic.style.display = "none";
+monthlyTraffic.style.display = "none";
+
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    let items = list.children;
+    let li = e.target;
+
+    for (let i = 0; i < items.length; i++) {
+      if (li.innerHTML == items[i].innerHTML) {
+        // document.getElementById(li.innerHTML + "-traffic").style.display="block";
+        document.getElementsByTagName("canvas")[i].style.display="block";
+      } else {
+        document.getElementsByTagName("canvas")[i].style.display="none";
+      }
+    }
+  }
+
+});
+
+let canvas = document.getElementsByTagName("canvas");
+
+for (let i = 0; i < canvas.length; i++) {
+  console.log(document.getElementsByTagName("canvas")[i]);
+}
